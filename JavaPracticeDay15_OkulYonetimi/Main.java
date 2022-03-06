4e920c6ad70d5a67a0604b34733e743eb1e83370
Enes Arslan <omerfurkanarslan18@gmail.com>
2022-03-07 01:19:00 +0300

    sekiz

---
+++ JavaPracticeDay15_OkulYonetimi/Main.java
@@ -0,0 +1,12 @@
+package JavaPracticeDay15_OkulYonetimi;
+
+import static JavaPracticeDay15_OkulYonetimi.Islemler.anaMenu;
+
+public class Main {
+    public static void main(String[] args) {
+       // Islemler islemler = new Islemler(); // obj create edildi
+       anaMenu(); //Islemler class import edilerek  obj create edilmeden anaMenu() method call edildi.
+    }
+
+
+}
